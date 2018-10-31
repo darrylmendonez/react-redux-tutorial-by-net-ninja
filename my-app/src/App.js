46ca955e782dc6928ba0ec39c851d008ceedad86
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-10-31 15:31:59 -0400

    nested Ninjas component into root component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
-        <p>Welcome ;)</p>
+        <p>Welcome :)</p>
+        <Ninjas />
       </div>
     );
   }
